60f4f5e7773f074e7161faf5c242474b7b97cf34
Jonathan-Agreda <flavioagreda1990@gmail.com>
2023-10-10 22:33:57 -0500

    MNOHILO AND ENVIRONMENT VARIABLE

---
+++ conceptos/monohilo.js
@@ -0,0 +1,12 @@
+console.log('Hola Jonas');
+
+let i = 0;
+setInterval(() => {
+    console.log(i);
+    i++;
+    /* if (i === 5) {
+        var a = 3 + z;
+    } */
+}, 1000);
+
+console.log('Segunda instrucción');
